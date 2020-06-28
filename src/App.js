846ac6ef114b57fc18eb7b8435969135f486fd30
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-06-28 21:11:43 +0800

    Update file path for NavBar component and merge App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import NavBar from './components/NavBar'
-class App extends Component {
-  render() {
+import NavBar from './NavBar'
+import FileUpload from './FileUpload'
+function App() {
     return (
       <div>
         <NavBar />
+        <FileUpload/>
       </div>
     )
-  }
 }
 export default App
