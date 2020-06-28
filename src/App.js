d046051f395e6d92ebd8f7e42b33573dc411cb58
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-06-29 05:40:57 +0800

    Make File Upload show only on File Upload Tab and update name

--- src/App.js
+++ src/App.js
@@ -8,11 +8,33 @@ function App() {
     // Function that is to be passed into MenuDraw to handles tab selections
     let handleTabSelection = function (text, index){
         console.log(`In App Root Component; Text: ${text}, Index: ${index}`)
+        setCurrentPageIndex(index)
     }
 
     return (
+
         <div>
-        <FileUpload name="Imperial iGEM 2020" sideBarCategories={sideBarCategories} handleTabSelection={handleTabSelection}/>
+            {currentPageIndex === 0 &&
+            <MenuDraw name="Imperial iGEM 2020 – Home" sideBarCategories={sideBarCategories}
+                      handleTabSelection={handleTabSelection} />
+            }
+            {currentPageIndex === 1 &&
+            <MenuDraw name="Imperial iGEM 2020 – Gene Design" sideBarCategories={sideBarCategories}
+                      handleTabSelection={handleTabSelection} />
+            }
+            {currentPageIndex === 2 &&
+            <MenuDraw name="Imperial iGEM 2020 – Combinatorial Specifications" sideBarCategories={sideBarCategories}
+                      handleTabSelection={handleTabSelection} />
+            }
+            {currentPageIndex === 3 &&
+            <MenuDraw name="Imperial iGEM 2020 – Generate Protocol" sideBarCategories={sideBarCategories}
+                      handleTabSelection={handleTabSelection} />
+            }
+            {currentPageIndex === 4 &&
+            <FileUpload name="Imperial iGEM 2020 – File Upload" sideBarCategories={sideBarCategories}
+                        handleTabSelection={handleTabSelection}/>
+            }
+
         </div>
     )
 }
