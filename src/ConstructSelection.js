aa1d997854d2ceba0db1ac689803eafb6d74d416
emmaalbertini <56163778+emmaalbertini@users.noreply.github.com>
2020-06-30 17:39:55 +0100

    s

--- src/ConstructSelection.js
+++ src/ConstructSelection.js
@@ -2,13 +2,13 @@ import React, {useState, useEffect} from 'react'
 import {Paper, Typography} from "@material-ui/core";
 
 
-export default function ConstructViewer(props){
+export default function ConstructSelection(props){
 
 
     return(
         <Paper elevation={3}>
             <Typography variant="h1" component="h2">
-                Construct Viewer
+                Construct Selection
             </Typography>
         </Paper>
     )
