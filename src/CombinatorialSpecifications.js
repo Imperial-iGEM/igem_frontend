31808305a2c579ff6a1df44c5c778782b08e28ae
emmaalbertini <56163778+emmaalbertini@users.noreply.github.com>
2020-06-30 11:56:56 +0100

    Update CombinatorialSpecifications.js

--- src/CombinatorialSpecifications.js
+++ src/CombinatorialSpecifications.js
@@ -1,27 +1,50 @@
 import React, {useState, useEffect} from 'react'
 import {Redirect, useLocation} from "react-router-dom";
 import MenuDraw from "./MenuDraw";
+import PartsSelection from "./PartsSelection";
+import ConstructViewer from "./ConstructViewer";
+import AssemblyMethod from "./AssemblyMethod ";
+import Annotations from "./Annotations ";
+import {makeStyles} from "@material-ui/core/styles";
 
 
-export default function CombinatorialSpecifications(props) {
+export default function GeneDesign(props) {
     const [navigate, setNavigate] = useState(false);
     const [navigateTo, setNavigateTo] = useState({})
     let currentLocation = useLocation();
+    const classes = useStyles();
 
     //Custom function to intercept tab selection to allow for saving of data etc before changing tabs.
     let handleTabSelection = function (location, index) {
-        console.log(`In CombinatorialSpecifications Component; Text: ${location.text}, Path: ${location.path}, Index: ${index}`)
-        setNavigateTo({path: location.path, push: true, state: {referrer: currentLocation}})
-        setNavigate(true)
-    }
-    if (navigate) {
-        return (<Redirect
-            to={{
-                pathname: navigateTo.path,
-                push: navigateTo.push,
-                state: navigateTo.state
+@ -23,5 +28,28 @@ export default function GeneDesign(props) {
             }}
         />)
     }
     return (<MenuDraw {...props} handleTabSelection={handleTabSelection}/>)
+
+
+    return (
+        <div>
+            <MenuDraw {...props} handleTabSelection={handleTabSelection}/>
+            <div className={classes.root}>
+                <PartsSelection/>
+                <ConstructViewer/>
+                <AssemblyMethod/>
+		<Annotations/>
+
+            </div>
+        </div>
+    )
 }
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'row',
+        minHeight: 600,
+        justifyContent: 'center',
+        margin: 20
+    }
+}));
