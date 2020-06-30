0614be4ccbcd58950189ab530e0dbd1e70f03265
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-06-30 18:16:41 +0800

    Make basic columns on Gene Design

--- src/GeneDesign.js
+++ src/GeneDesign.js
@@ -1,12 +1,17 @@
 import React, {useState, useEffect} from 'react'
 import {Redirect, useLocation} from "react-router-dom";
 import MenuDraw from "./MenuDraw";
+import PartsList from "./PartsList";
+import ConstructViewer from "./ConstructViewer";
+import PartsInfo from "./PartsInfo";
+import {makeStyles} from "@material-ui/core/styles";
 
 
 export default function GeneDesign(props) {
     const [navigate, setNavigate] = useState(false);
     const [navigateTo, setNavigateTo] = useState({})
     let currentLocation = useLocation();
+    const classes = useStyles();
 
     //Custom function to intercept tab selection to allow for saving of data etc before changing tabs.
     let handleTabSelection = function (location, index) {
@@ -23,5 +28,28 @@ export default function GeneDesign(props) {
             }}
         />)
     }
-    return (<MenuDraw {...props} handleTabSelection={handleTabSelection}/>)
+
+
+    return (
+        <div>
+            <MenuDraw {...props} handleTabSelection={handleTabSelection}/>
+            <div className={classes.root}>
+                <PartsList/>
+                <ConstructViewer/>
+                <PartsInfo/>
+            </div>
+        </div>
+    )
 }
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'row',
+        minHeight: 600,
+        justifyContent: 'center',
+        margin: 20
+    }
+}));
---
+++ src/PartsList.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react'
+import {Paper, Typography} from '@material-ui/core'
+
+
+export default function PartsList(props){
+
+
+    return(
+        <Paper elevation={3}>
+            <Typography variant="h1" component="h2">
+                Parts List
+            </Typography>
+        </Paper>
+    )
+}
