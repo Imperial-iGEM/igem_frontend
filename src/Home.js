25e862ee4524dc8827693f932b13f867dc41bbf8
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-07-15 06:28:34 +0800

    Untergrate SBOL designer into frontend Home Page

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,53 @@
 import React, {useState, useEffect} from 'react'
 import {Redirect, useLocation} from "react-router-dom";
 import MenuDraw from "./MenuDraw";
-
+import useScript from "./useScript";
 
 export default function Home(props) {
     const [navigate, setNavigate] = useState(false);
     const [navigateTo, setNavigateTo] = useState({})
     let currentLocation = useLocation();
+    const webSwingScriptOne =
+        " var webswingInstance0 = {\n" +
+        "        options: {\n" +
+        "            autoStart: true,\n" +
+        "            recording: getParam('recording'),\n" +
+        "            binarySocket: getParam('binarySocket'),\n" +
+        "            debugPort: getParam('debugPort'),\n" +
+        "            recordingPlayback: getParam('recordingPlayback'),\n" +
+        "            connectionUrl:'https://www.imperialigem2020.live/sboldesigner' \n" +
+        "        }\n" +
+        "    }\n" +
+        "\n" +
+        "    function getParam(name) {\n" +
+        "        name = name.replace(/[\\[]/, \"\\\\\\[\").replace(/[\\]]/, \"\\\\\\]\");\n" +
+        "        var results = new RegExp(\"[\\\\?&]\" + name + \"=([^&#]*)\").exec(location.href);\n" +
+        "        return results == null ? null : decodeURIComponent(results[1]);\n" +
+        "    }"
+
+    const webSwingScriptTwo =
+        "(function (window, document) {\n" +
+        "        var loader = function () {\n" +
+        "            var baseUrl = 'https://www.imperialigem2020.live/sboldesigner';\n" +
+        "            baseUrl = baseUrl.indexOf(\"/\", baseUrl.length - 1) !== -1 ? baseUrl : (baseUrl + \"/\");\n" +
+        "            var xmlhttp = new XMLHttpRequest();\n" +
+        "            xmlhttp.onreadystatechange = function () {\n" +
+        "                if (xmlhttp.readyState == XMLHttpRequest.DONE) {\n" +
+        "                    var version = xmlhttp.status == 200 ? xmlhttp.responseText : \"undefined\";\n" +
+        "                    var script = document.createElement(\"script\"),\n" +
+        "                        tag = document.getElementsByTagName(\"script\")[0];\n" +
+        "                    script.src = baseUrl + \"javascript/webswing-embed.js?version=\" + version;\n" +
+        "                    tag.parentNode.insertBefore(script, tag);\n" +
+        "                }\n" +
+        "            };\n" +
+        "            xmlhttp.open(\"GET\", baseUrl + \"rest/version\", true);\n" +
+        "            xmlhttp.send();\n" +
+        "        };\n" +
+        "        window.addEventListener ? window.addEventListener(\"load\", loader, false) : window.attachEvent(\"onload\", loader);\n" +
+        "    })(window, document);"
+    const webSwingScriptTwoAttributes = {"data-webswing-global-var": "webswing"};
+    useScript(webSwingScriptOne, {}, false, false);
+    useScript(webSwingScriptTwo, webSwingScriptTwoAttributes, false, false);
 
     //Custom function to intercept tab selection to allow for saving of data etc before changing tabs.
     let handleTabSelection = function (location, index) {
@@ -23,5 +64,9 @@ export default function Home(props) {
             }}
         />)
     }
-    return (<MenuDraw {...props} handleTabSelection={handleTabSelection}/>)
+    return (
+        [<MenuDraw {...props} handleTabSelection={handleTabSelection}/>,
+            <div className="webswing-element" data-webswing-instance="webswingInstance0" style={{height: '100vh'}}>
+            </div>]
+    )
 }
---
+++ src/useScript.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+const useScript = (scriptContent, attributes = {}, async = false, defer = false) => {
+    useEffect(() => {
+        const script = document.createElement('script');
+
+        script.async = async;
+        script.defer = defer;
+        script.innerHTML = scriptContent;
+
+        document.body.appendChild(script);
+
+        for (let key of Object.keys(attributes)) {
+            script.setAttribute(key.toString(), attributes[key].toString())
+            console.log(key + " -> " + attributes[key])
+        }
+
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, [scriptContent]);
+};
+
+export default useScript;
