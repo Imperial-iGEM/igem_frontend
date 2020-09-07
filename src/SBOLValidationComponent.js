d2c4b7215ca961b8229c4e01b02df2a3640a61ae
Raymond Cadoc Miles <raymond.c.miles@gmail.com>
2020-09-07 18:18:59 +0200

    FileUpload together with SBOL validation incorporated to the SimpleTabs component

---
+++ src/SBOLValidationComponent.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+import {Card, CardContent, Paper, Typography} from "@material-ui/core";
+import { spacing } from '@material-ui/system';
+import Button from "@material-ui/core/Button";
+
+import Grid from "@material-ui/core/Grid";
+export default function SBOLValidationComponent(props) {
+
+
+    return (
+        <div >
+            <Paper style={{padding:20, marginTop:10, marginBottom:10}} >
+                <Typography variant="h6"  style = {{padding:20}} id={"errorBox"} >
+                    Errors
+                </Typography>
+            </Paper>
+            <Paper style={{padding:20, marginTop:10, marginBottom:10}}>
+
+                <Typography variant="h6"  style = {{padding:20}}  id={"outPutFile"}>
+                    OutputFile
+                </Typography>
+
+
+
+            </Paper>
+            <Grid container
+                  direction="column-reverse"
+                  justify="center"
+                  alignItems="center">
+                <Grid item xl>
+                    <Button variant="contained" color="primary" centerRipple={3}>
+                        Info
+                    </Button>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
