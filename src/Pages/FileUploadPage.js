f801b0d4a2a9ccdc85b7343a18598cc73888a33d
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-10-07 10:31:12 +0100

    adds DNA assebmly selection button

--- src/Pages/FileUploadPage.js
+++ src/Pages/FileUploadPage.js
@@ -5,13 +5,32 @@ import FileUpload from "../SBOLValidation/FileUpload";
 import TutorialButton from "../SBOLValidation/TutorialButton"
 import {Redirect, useLocation} from "react-router-dom";
 
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
 const useStyles = makeStyles({
     root: {
         flexGrow: 1,
     },
+    radioAssembly: {
+        textAlign: 'center',
+        padding: '15px',
+    }
 });
 
 export default function FileUploadPage(props) {
+    const classes = useStyles();
+    
+    // To Use Radio Buttons
+    const [value, setValue] = React.useState('basic');
+
+    // To Control Radio buttons
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
     //const classes = useStyles();
     const [selectedTab, setSelectedTab] = React.useState(0);
     const [navigate, setNavigate] = useState(false);
@@ -21,7 +40,7 @@ export default function FileUploadPage(props) {
 //the value is which tab is selected
 
 
-    const handleChange = (event, newValue) => {
+    const handleTabChange = (event, newValue) => {
         setSelectedTab(newValue);
     }; // Triggered whenever we take a new value
 
@@ -43,32 +62,50 @@ export default function FileUploadPage(props) {
     }
 
     return (
-
-        <>
+        <div>
             <AppBar position ="static" style={{ background: '#C4C4C4' }}>
-
                 <Tabs
                 value={selectedTab}
-                onChange={handleChange}
+                onChange={handleTabChange}
                 indicatorColor="#FF6F90"
                 textColor="primary"
                 centered
                 variant="standard">
-
-                <Tab label="Upload File"  title={"Upload to SBOLDesigner without validating"}>
-                </Tab>
-
-                <Tab label="Upload File and Validate" title={"Upload to SBOLDesigner after validating"}>
-                </Tab>
-
-            </Tabs>
+                    <Tab label="Upload File"  title={"Upload to SBOLDesigner without validating"} />
+                    <Tab label="Upload File and Validate" title={"Upload to SBOLDesigner after validating"} />
+                </Tabs>
 
             </AppBar>
-            {selectedTab === 1 && <SBOLValidator/>}
-            {selectedTab === 0 && <FileUpload/>}
-        <div>
-            <TutorialButton></TutorialButton>
+            <div className={classes.radioAssembly}>
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">Which DNA Assembly method would you like to use?</FormLabel>
+                    <RadioGroup aria-label="position" name="position" defaultValue="top">
+                        <FormControlLabel
+                        value="basic"
+                        control={<Radio color="primary" />}
+                        label="Basic"
+                        labelPlacement="right"
+                        />
+                        <FormControlLabel
+                        value="moclo"
+                        control={<Radio color="primary" />}
+                        label="Mo Clo"
+                        labelPlacement="right"
+                        />
+                        <FormControlLabel
+                        value="goldengate"
+                        control={<Radio color="primary" />}
+                        label="Golden Gate"
+                        labelPlacement="right"
+                        />
+                    </RadioGroup>
+                </FormControl>
+            </div>
+                {selectedTab === 1 && <SBOLValidator/>}
+                {selectedTab === 0 && <FileUpload/>}
+            <div>
+                <TutorialButton></TutorialButton>
+            </div>
         </div>
-            </>
     );
 }
