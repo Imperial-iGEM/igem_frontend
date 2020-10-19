d71567f1233ebd9d1522aa6a6bf779743b59115e
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-10-19 11:05:14 +0100

    adds button min height

--- src/SBOLValidation/DownloadTemplate.js
+++ src/SBOLValidation/DownloadTemplate.js
@@ -7,7 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     myButton: {
       width: '100%',
-      minHeight: '50px'
+      minHeight: '100px'
     },
     myGrid: {
         padding: '10px'
