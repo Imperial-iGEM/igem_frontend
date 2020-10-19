6665f7f810f6171cb128cfd43ee2e12cdca7a0c7
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-10-19 11:21:01 +0100

    adds success messages on download

--- src/SBOLValidation/DownloadTemplate.js
+++ src/SBOLValidation/DownloadTemplate.js
@@ -3,6 +3,8 @@ import {Button} from "@material-ui/core";
 import Grid from '@material-ui/core/Grid';
 import {withStyles} from "@material-ui/styles";
 import { makeStyles } from '@material-ui/core/styles';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles((theme) => ({
     myButton: {
@@ -14,9 +16,51 @@ const useStyles = makeStyles((theme) => ({
     }
   }));
 
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
 export default function DownloadTemplate(props){
 
     const classes = useStyles();
+
+    const [openBasic, setOpenBasic] = React.useState(false);
+    const [openMoclo, setOpenMoclo] = React.useState(false);
+    const [openBiobricks, setOpenBiobricks] = React.useState(false);
+
+    const handleClickBasic = () => {
+        setOpenBasic(true);
+    };
+    const handleClickMoclo = () => {
+        setOpenMoclo(true);
+    };
+    const handleClickBiobricks = () => {
+        setOpenBiobricks(true);
+    };
+
+    const handleCloseBasic = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+
+        setOpenBasic(false);
+    };
+
+    const handleCloseMoclo = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+
+        setOpenMoclo(false);
+    };
+
+    const handleCloseBiobricks = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+
+        setOpenBiobricks(false);
+    };
 
     return(
         <Grid
@@ -29,24 +73,39 @@ export default function DownloadTemplate(props){
         >
             <Grid item xs={12} sm={4} >
                 <a href={process.env.PUBLIC_URL + 'SBOL/basic_validation.xml'} download style={{ textDecoration: 'none' }}>
-                    <Button className={classes.myButton} variant="outlined" color="secondary">
+                    <Button onClick={handleClickBasic} className={classes.myButton} variant="outlined" color="secondary">
                         Basic
                     </Button>
                 </a>
+                <Snackbar open={openBasic} autoHideDuration={6000} onClose={handleCloseBasic}>
+                    <Alert onClose={handleCloseBasic} severity="success">
+                        Successfully Downloaded example Basic assembly method SBOL File
+                    </Alert>
+                </Snackbar>
             </Grid>
             <Grid item xs={12} sm={4}>
                 <a href={process.env.PUBLIC_URL + 'SBOL/moclo_validation.xml'} download style={{ textDecoration: 'none' }}>
-                    <Button className={classes.myButton} variant="outlined" color="secondary">
+                    <Button onClick={handleClickMoclo} className={classes.myButton} variant="outlined" color="secondary">
                         MoClo (Golden Gate)
                     </Button>
                 </a>
+                <Snackbar open={openMoclo} autoHideDuration={6000} onClose={handleCloseMoclo}>
+                    <Alert onClose={handleCloseMoclo} severity="success">
+                        Successfully Downloaded example MoClo assembly method SBOL File
+                    </Alert>
+                </Snackbar>
             </Grid>
             <Grid item xs={12} sm={4}>
                 <a href={process.env.PUBLIC_URL + 'SBOL/bb_validation_level1.xml'} download style={{ textDecoration: 'none' }}>
-                    <Button className={classes.myButton} variant="outlined" color="secondary">
+                    <Button onClick={handleClickBiobricks} className={classes.myButton} variant="outlined" color="secondary">
                         BioBricks
                     </Button>
                 </a>
+                <Snackbar open={openBiobricks} autoHideDuration={6000} onClose={handleCloseBiobricks}>
+                    <Alert onClose={handleCloseBiobricks} severity="success">
+                        Successfully Downloaded example BioBricks assembly method SBOL File
+                    </Alert>
+                </Snackbar>
             </Grid>
         </Grid>
     )
