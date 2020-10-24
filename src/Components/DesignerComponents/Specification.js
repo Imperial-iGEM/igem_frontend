d4cd840b563dc88d7d2f862a7b52d3c0f56802aa
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-10-25 02:20:44 +0800

    add state setter method

--- src/Components/DesignerComponents/Specification.js
+++ src/Components/DesignerComponents/Specification.js
@@ -196,7 +196,7 @@ export default function ExampleSpecification(props) {
   const [maxNumWellPerPlate, setMaxNumWellPerPlate] = useState('');
   const [numRunPerPlate, setNumRunPerPlate] = useState('');
 
-  const [wellLabels96 ] = useState(['A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12',
+  const [wellLabels96, setWellLabels96] = useState(['A1','A2','A3','A4','A5','A6','A7','A8','A9','A10','A11','A12',
                                     'B1','B2','B3','B4','B5','B6','B7','B9','B9','B10','B11','B12',
                                     'C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12',
                                     'D1','D2','D3','D4','D5','D6','D7','D8','D9','D10','D11','D12',
